bc2df1f8433289110d0409cde578581d6e616dc0
Zakaria SMAHI <zakaria08esi@gmail.com>
2013-12-20 19:30:21 +0100

    adding hash class

---
+++ Ekhbesh/Hash.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Hash
+{
+    
+    public Hash()
+    {
+            
+    }
+    public string pass { get; set; }
+
+    public string getMD5()
+    {
+        // Create a new instance of the MD5CryptoServiceProvider object.
+        MD5 md5Hasher = MD5.Create();
+
+        // Convert the input string to a byte array and compute the hash.
+        byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+        // Create a new Stringbuilder to collect the bytes
+        // and create a string.
+        StringBuilder sBuilder = new StringBuilder();
+
+        // Loop through each byte of the hashed data 
+        // and format each one as a hexadecimal string.
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+
+        // Return the hexadecimal string.
+        return sBuilder.ToString();
+    }
+    public bool verifyMd5Hash(string input, string hash)
+    {
+        ///
+        /// verify the hash using XPATH request
+        /// 
+        return true;
+    }
+
+
+}
